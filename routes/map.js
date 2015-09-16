fddf1f2738199df40045ff232ed7ac18d48b5c70
Xindong Ding <dxd.spirits@gmail.com>
2015-09-17 01:58:25 +0800

    modify routes for map

--- routes/map.js
+++ routes/map.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.redirect('http://www.wedfairy.com/');
+router.get('/', function(req, res, next) {
+    // res.redirect('http://www.wedfairy.com/');
+    next();
 });
 
 router.get('/:map_id/settings/', function(req, res, next) {
@@ -23,7 +24,7 @@ router.get('/mark/', function(req, res, next) {
     res.render('map/mark', {});
 });
 
-router.get('/:map_id/show/', function(req, res, next) {
+router.get('/:map_id/', function(req, res, next) {
     var map_id = req.params.map_id;
     res.render('map/show', {
         map_id: map_id
