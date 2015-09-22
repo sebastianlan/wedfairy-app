54cb6e0118493631cd69352ccffe81f0340e5600
sebastianlan <sebastianlan.original@gmail.com>
2015-09-23 01:48:45 +0800

    modify stylesheets & add data mobile

--- public/javascripts/global_tool.js
+++ public/javascripts/global_tool.js
@@ -1,9 +1,24 @@
 // 转义字符 "\n" 转换为 html 标签 <br/>
 function return2br(str){
-    return str.replace(/\r?\n/g,"<br/>");
+    if(str == null||str == ""){
+        return "";
+    }else{
+        return str.replace(/\r?\n/g,"<br/>");
+    }
 }
 //获取当前日期函数
 function get_current_date(){
     var date = new Date();
-    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    var current_year = date.getFullYear();
+    if(date.getMonth() < 9){
+        var current_month = "0" + (date.getMonth() + 1);
+    }else{
+        var current_month = date.getMonth() + 1;
+    }
+    if(date.getDate() < 10){
+        var current_date = "0" + date.getDate();
+    }else{
+        var current_date = date.getDate();
+    }
+    return current_year + "-" + current_month + "-" + current_date;
 }
--- settings/settings.js
+++ settings/settings.js
@@ -3,21 +3,9 @@ var CONFIG = {
 
     TAT_STATIC : 'http://static.wedfairy.com/',
     VU_API_ROOT :'http://api.wedfairy.com/api/users/user/',
-
     COMPOSER_HOST : 'http://192.168.0.169:3000/',
-
     API_ROOT : 'http://192.168.0.169:8000/api/',
-    API_HOST : 'http://192.168.0.169:8000/api/',
-    API_HOSTNAME : 'api.wedfairy.com',
-    API_PORT : 8000,
-    API_PROTOCOL : 'http',
-
-    CDN_URL : '/public/',
-
-    WECHAT: {
-        APPID: '',
-        APPSECRET: ''
-    }
+    CDN_URL : '/public/'
 };
 
 try {
